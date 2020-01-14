a070742123ae439a9d5999bff275fdc47d30d253
Masoud Hosseini <massoud.hosseini@gmail.com>
2020-01-14 02:31:15 -0500

    Added Rest API routes for user and auction (No route is implemented)

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,17 +2,19 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('/register', 'UserController@create');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/users', 'UserController@index');                               //All Users
+Route::get('/auctions', 'AuctionController@index');                         //All Auctions
+
+Route::get('/user/{id}', 'UserController@show');                            //User
+Route::get('/user/{id}/auctions', 'UserController@auctions');               //User Auctions
+Route::get('/user/{id}/bids', 'UserController@bids');                       //User bids
+
+Route::get('/auction/{id}', 'AuctionController@show');                      //Auction
+Route::get('/auction/{id}/bids', 'AuctionController@bids');                 //All auction bids
+Route::get('/auction/{id}/highestBid', 'AuctionController@highestBid');     //Auction win bid
+
+Route::post('/auction', 'AuctionController@create');                        //Creating Auction
+Route::delete('/auction/{id}', 'AuctionController@destroy');                //Deleting Auction
+Route::put('/auction/{id}', 'AuctionController@bid');                       //Creating Bid
